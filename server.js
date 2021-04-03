4b117c61808d7a381e89a4457ee59091da4fdaf5
zakrepashe <zakrepashe@gmail.com>
2021-04-03 19:24:12 +0300

    Add CORS config for ngrok

--- server.js
+++ server.js
@@ -11,12 +11,26 @@ const server = app.listen(PORT, () => {
 });
 
 // socket.io
-const io = ioLib(server);
+const io = ioLib(server, {
+  cors: {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  },
+});
 
 // views
 app.set('views', path.join(__dirname, 'dist'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'dist')));
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('*', (req, res) => {
   res.render('index');
